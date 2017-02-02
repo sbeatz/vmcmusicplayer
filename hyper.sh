9e597610d6c964b78ac0986c5753a010837ab880
sbeatz <sbeatz76@googlemail.com>
2017-02-02 20:09:28 +0100

    Create hyper.sh

---
+++ hyper.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          hyperion-remote
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: hyperion-remote
+# Description:       hyperion-remote server
+### END INIT INFO
+python /home/pi/hs.py &
